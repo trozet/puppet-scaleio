247f56bb0dba092380dd498e860338bc08db6fd1
alexandrelevine <alexandrelevine@gmail.com>
2016-03-20 17:56:39 +0300

    Idempotent SDC installation

--- manifests/sdc_server.pp
+++ manifests/sdc_server.pp
@@ -1,6 +1,6 @@
 class scaleio::sdc_server (
   $ensure  = 'present',
-  $mdm_ip  = undef,
+  $mdm_ip  = undef,     # "1.2.3.4,1.2.3.5"
   )
 {
   package { ['numactl', 'libaio1']:
@@ -12,8 +12,9 @@ class scaleio::sdc_server (
   if $mdm_ip {
     exec { 'connect to mdm':
       command => "drv_cfg --add_mdm --ip ${mdm_ip}",
-      path => '/opt/emc/scaleio/sdc/bin',
-      require => Package['emc-scaleio-sdc']}
+      path => '/opt/emc/scaleio/sdc/bin:/bin',
+      require => Package['emc-scaleio-sdc'],
+      onlyif => "drv_cfg --query_mdms | grep 'Retrieved 0'"}
   }
 
   # TODO:
