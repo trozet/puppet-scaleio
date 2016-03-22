c2092944fe02da157f2acdec7e7073f58ce6da0d
alexandrelevine <alexandrelevine@gmail.com>
2016-03-22 18:40:27 +0300

    Fixed another naming conflict

--- manifests/mdm.pp
+++ manifests/mdm.pp
@@ -23,7 +23,7 @@ define scaleio::mdm (
   }
 
   if $management_ips {
-    cmd {$ensure_properties:
+    cmd {"properties ${ensure_properties}":
       action => 'modify_management_ip', ref => 'target_mdm_name', value => $name,
       extra_opts => "--new_mdm_management_ip ${management_ips}"}
   }
