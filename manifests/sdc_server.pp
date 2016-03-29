02ba4ad6afe4bf4be5b3cfcd5c0012ae28295c4b
alexandrelevine <alexandrelevine@gmail.com>
2016-03-29 19:20:10 +0300

    Allows addition of more IPs

--- manifests/sdc_server.pp
+++ manifests/sdc_server.pp
@@ -5,6 +5,15 @@ class scaleio::sdc_server (
   $mdm_ip  = undef,     # string - List of MDM IPs
   )
 {
+  define add_ip {
+    exec { "add ip ${title}":
+	    command => "drv_cfg --add_mdm --ip ${title}",
+	    path => '/opt/emc/scaleio/sdc/bin:/bin',
+	    require => Package['emc-scaleio-sdc'],
+	    unless => "drv_cfg --query_mdms | grep ${title}"
+	  } 
+	}
+  
   package { ['numactl', 'libaio1']:
     ensure => installed,
   } ->
@@ -12,12 +21,11 @@ class scaleio::sdc_server (
     ensure => $ensure,
   }
   if $mdm_ip {
-    exec { 'connect to mdm':
-      command => "drv_cfg --add_mdm --ip ${mdm_ip}",
-      path => '/opt/emc/scaleio/sdc/bin:/bin',
-      require => Package['emc-scaleio-sdc'],
-      onlyif => "drv_cfg --query_mdms | grep 'Retrieved 0'"
-    } ->
+    $ip_array = split($mdm_ip, ',')
+
+    if $ensure == 'present' {
+        add_ip { $ip_array: }
+    }
     file_line { 'Set MDM IP addresses in drv_cfg.txt':
       ensure  => present,
       line    => "mdm ${mdm_ip}",
