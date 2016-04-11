af0873d26799a589c5353594345a371375f833e5
alexey-mr <alexey.morlang@gmail.com>
2016-04-11 18:49:09 +0300

    Set noop io scheduler for SDS/flash devices

--- manifests/sds_server.pp
+++ manifests/sds_server.pp
@@ -14,8 +14,13 @@ class scaleio::sds_server (
   } ->
   package { ['emc-scaleio-sds']:
     ensure => $ensure,
+  } ->
+  exec { 'Apply noop IO scheduler for SSD/flash disks':
+    command   => 'bash -c \'for i in `lsblk -d -o ROTA,KNAME | awk "/^ *0/ {print($2)}"` ; do if [ -f /sys/block/$i/queue/scheduler ]; then echo noop > /sys/block/$i/queue/scheduler; fi; done\'',
+    path      => '/bin:/usr/bin',
   }
-
+  
+  
   # TODO:
   # "absent" cleanup
 }
