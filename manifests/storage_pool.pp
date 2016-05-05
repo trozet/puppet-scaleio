93d36b43f614f9f254e84cc45c6e78f9a85379f2
alexey-mr <alexey.morlang@gmail.com>
2016-05-05 16:14:02 +0300

    Fixed resource duplication in case of multiple storage pools

--- manifests/storage_pool.pp
+++ manifests/storage_pool.pp
@@ -17,41 +17,41 @@ define scaleio::storage_pool (
   $rebalance_parallelism_limit    = undef,      # int
   )
 {
-  cmd {$ensure:
+  cmd {"storage pool ${name} ${ensure}":
     action        => $ensure,
     entity        => 'storage_pool',
     value         => $name,
     scope_entity  => 'protection_domain',
     scope_value   => $protection_domain}
 
-  set { 'set_checksum_mode':
+  set { "storage pool ${name} set_checksum_mode":
     is_defined  => $checksum_mode,
     change      => "--${checksum_mode}_checksum"}
-  set { 'set_rebuild_mode':
+  set { "storage pool ${name} set_rebuild_mode":
     is_defined  => $rebuild_mode,
     change      => "--${rebuild_mode}_rebuild --i_am_sure"}
-  set { 'set_rebalance_mode':
+  set { "storage pool ${name} set_rebalance_mode":
     is_defined  => $rebalance_mode,
     change      => "--${rebalance_mode}_rebalance --i_am_sure"}
-  set { 'modify_zero_padding_policy':
+  set { "storage pool ${name} modify_zero_padding_policy":
     is_defined  => $zero_padding_policy,
     change      => "--${zero_padding_policy}_zero_padding"}
-  set { 'set_rmcache_write_handling_mode':
+  set { "storage pool ${name} set_rmcache_write_handling_mode":
     is_defined  => $rmcache_write_handling_mode,
     change      => "--rmcache_write_handling_mode ${rmcache_write_handling_mode} --i_am_sure"}
-  set { 'set_rmcache_usage':
+  set { "storage pool ${name} set_rmcache_usage":
     is_defined  => $rmcache_usage,
     change      => "--${rmcache_usage}_rmcache --i_am_sure"}
-  set { 'modify_spare_policy':
+  set { "storage pool ${name} modify_spare_policy":
     is_defined  => $spare_percentage,
     change      => "--spare_percentage ${spare_percentage} --i_am_sure"}
-  set { 'set_rebuild_rebalance_parallelism':
+  set { "storage pool ${name} set_rebuild_rebalance_parallelism":
     is_defined  => $rebalance_parallelism_limit,
     change      => "--limit ${rebalance_parallelism_limit}"}
   $scanner_mode_change = $scanner_mode ? {
     'disable'   => "--scanner_mode ${scanner_mode} --scanner_bandwidth_limit ${scanner_bandwidth_limit}",
     default     => ' '}
-  set { "${scanner_mode}_background_device_scanner":
+  set { "storage pool ${name} ${scanner_mode}_background_device_scanner":
     is_defined  => $scanner_mode != '',
     change      => $scanner_mode_change }
 
